f0f461a50ffc5257eee79c43e89056f2078259fa
SukeshKaicharla <141485656+SukeshKaicharla@users.noreply.github.com>
2025-09-16 20:34:07 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
 export default defineConfig({
-  base: "/Portfolio/",
+  base: "./",   // 👈 relative paths, works when deployed at /
   plugins: [react()],
   resolve: {
     alias: {
@@ -12,6 +11,7 @@ export default defineConfig({
     },
   },
 });
+
 
 
   plugins: [
